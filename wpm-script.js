3baee05ac96b483e473ed633e8d4703d84b6be63
maillefaud.florent@gmail.com <maillefaud.florent@gmail.com@78bca48e-3cea-953d-1f40-5f63cba3b049>
2014-08-09 13:58:05 +0000

    New version tagging 2.0

--- wpm-script.js
+++ wpm-script.js
@@ -1,15 +1,66 @@
 jQuery(document).ready(function() {
 
-    jQuery('#upload_image_button').click(function() {
-        formfield = jQuery('#upload_image').attr('name');
-        tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
-        return false;
+    var custom_uploader;
+ 
+    jQuery('#upload_image_button').click(function(e) {
+ 
+        e.preventDefault();
+ 
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+ 
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: Data.title,
+            button: {
+                text: Data.textebutton
+            },
+            multiple: false
+        });
+ 
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function() {
+            attachment = custom_uploader.state().get('selection').first().toJSON();
+            jQuery('#upload_image').val(attachment.url);
+        });
+ 
+        //Open the uploader dialog
+        custom_uploader.open();
+ 
     });
-
-    window.send_to_editor = function(html) {
-        imgurl = jQuery('img',html).attr('src');
-        jQuery('#upload_image').val(imgurl);
-        tb_remove();
-    }
+    
+    jQuery('#upload_b_image_button').click(function(e) {
+ 
+        e.preventDefault();
+ 
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+ 
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: Data.title,
+            button: {
+                text: Data.textebutton
+            },
+            multiple: false
+        });
+ 
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function() {
+            attachment = custom_uploader.state().get('selection').first().toJSON();
+            jQuery('#upload_b_image').val(attachment.url);
+        });
+ 
+        //Open the uploader dialog
+        custom_uploader.open();
+ 
+    });
+    
 });
 
