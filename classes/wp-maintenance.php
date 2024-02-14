5afbf470bb9035298aa0c0c91489f0761eeb93c3
Florent Maillefaud <florent@restezconnectes.fr>
2024-02-14 17:39:32 +0100

    Disable REST API if not logged

--- classes/wp-maintenance.php
+++ classes/wp-maintenance.php
@@ -38,6 +38,29 @@ class WP_maintenance {
         }
         // disabled XMLRPC
         add_filter('xmlrpc_enabled', '__return_false');
+        /** Disable REST API **/
+        add_filter( 'rest_authentication_errors', function( $result ) {
+            // If a previous authentication check was applied,
+            // pass that result along without modification.
+            if ( true === $result || is_wp_error( $result ) ) {
+                return $result;
+            }
+        
+            // No authentication has been performed yet.
+            // Return an error if user is not logged in.
+            if ( ! is_user_logged_in() ) {
+                return new WP_Error(
+                    'rest_not_logged_in',
+                    __( 'You are not currently logged in.' ),
+                    array( 'status' => 401 )
+                );
+            }
+        
+            // Our custom authentication check should have no effect
+            // on logged-in requests
+            return $result;
+        });
+    
     }
 
     function wpm_theme_add_editor_styles() {
@@ -440,7 +463,7 @@ class WP_maintenance {
                 
             }
         }
-    }
+    }    
 
     function wpm_init_wysiwyg() {
         wp_enqueue_script('editor');
